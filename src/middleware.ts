8456d1319844dacc11b58bd46162c08b64a5fa25
beka <badagadzeb@gmail.com>
2024-07-24 01:24:12 +0400

    add storyBook

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import createMiddleware from "next-intl/middleware";
 import { locales, localePrefix } from "@/navigation";
 
 export default createMiddleware({
-  defaultLocale: locales[1],
+  defaultLocale: locales[0],
   localePrefix,
   locales,
 });
--- src/navigation.ts
+++ src/navigation.ts
@@ -1,7 +1,7 @@
 import { createSharedPathnamesNavigation } from "next-intl/navigation";
 
 export const locales = ["ka", "en"] as const;
-export const localePrefix = "always";
+export const localePrefix = "as-needed";
 
 const { Link, redirect, usePathname, useRouter } =
   createSharedPathnamesNavigation({ locales, localePrefix });
